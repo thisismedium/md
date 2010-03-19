1cbbaa99be0f20950418357ca814141c3c631edf
Ben Weaver <weaver@coptix.com>
2010-03-19 14:30:24 -0400

    Update stm.meronymy.

--- md/stm/meronymy.py
+++ md/stm/meronymy.py
@@ -3,47 +3,44 @@ from md import fluid
 from .interfaces import Cursor
 from .log import weaklog
 
-__all__ = ('partof', 'whole')
+__all__ = ('partof', 'whole', 'wholes', 'MeronymicError')
 
 def partof(whole, part):
     if isinstance(part, Cursor) and isinstance(whole, Cursor):
-	register_part(current_meronymy(), whole, part)
+        register_part(MERONYMY, whole, part)
     return part
 
-def whole(parts, default=None):
-    return resolve_whole(current_meronymy(), parts, default=None)
+def whole(part, default=None):
+    return MERONYMY.get(part, default)
+
+def wholes(parts, default=None):
+    return (MERONYMY.get(p, default) for p in parts)
 
 
 ###
 
 class MeronymicError(Exception): pass
 
-CURRENT_MERONYMY = fluid.cell(weaklog())
-
-def current_meronymy():
-    return CURRENT_MERONYMY.value
-
-def resolve_whole(meronymy, parts, default=None):
-    return ((p, meronymy.get(p, default)) for p in parts)
+MERONYMY = weaklog()
 
 def register_part(meronymy, whole, part):
     ## Resolve whole to a real whole if it is part of something else.
     whole = meronymy.get(whole, whole)
 
     try:
-	holon = meronymy[part]
-	if holon is whole:
-	    return
-	else:
-	    raise MeronymicError(
-		'whole=%r does not match existing holon=%r '
-		'for part=%r' % (whole, holon, part)
-	    )
+        holon = meronymy[part]
+        if holon is whole:
+            return
+        else:
+            raise MeronymicError(
+                'whole=%r does not match existing holon=%r '
+                'for part=%r' % (whole, holon, part)
+            )
     except KeyError:
-	try:
-	    meronymy.allocate(part, whole)
-	except ValueError:
-	    raise MeronymicError(
-		'%r is already part of %r '
-		'(not whole=%r)' % (part, meronomy[part], whole)
-	    )
+        try:
+            meronymy.allocate(part, whole)
+        except ValueError:
+            raise MeronymicError(
+                '%r is already part of %r '
+                '(not whole=%r)' % (part, meronomy[part], whole)
+            )
